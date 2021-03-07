2b71224aaa8b2d9199dd49335c6e865e44c2def8
Damilare James <damiadehjames@gmail.com>
2021-03-07 21:18:16 +0100

    hosting on netlify

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 
 const withVideos = require("next-videos");
 
-module.exports = withVideos();
+// module.exports = withVideos();
 module.exports = {
     // Target must be serverless
+    ...withVideos(),
     target: "serverless",
   };
