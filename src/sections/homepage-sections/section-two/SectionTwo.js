0a5a8a5aa81b08d2023735a0e7be565220873f40
OyinloluB <oyinloluwab@gmail.com>
2021-03-11 03:44:16 -0800

    .

--- src/sections/homepage-sections/section-two/SectionTwo.js
+++ src/sections/homepage-sections/section-two/SectionTwo.js
@@ -35,6 +35,21 @@ const littleCards = {
         "We create the optimal platform to develop and run digital applications for our clients.",
       backgroundColor: "#f5f8ff",
     },
+    {
+      icon: "/assets/home/section_two/code2.svg",
+      title: "Web Development",
+      text:
+        "In Nubitz, we help you create campanigns and content that drives traffic with our marketing strategies.",
+      textColor: "rgba(249, 249, 249, 0.8)",
+      titleColor: "#ffffff",
+    },
+    {
+      icon: "/assets/home/section_two/code1.svg",
+      title: "Cloud Development",
+      text:
+        "We create the optimal platform to develop and run digital applications for our clients.",
+      backgroundColor: "#f5f8ff",
+    },
   ],
 };
 
